b239c76b6c4a40e6b3871025a2444f1755521716
Maurizio Verduci <maurizioverduci@Air-di-Maurizio.lan>
2020-10-17 17:28:01 +0200

    File per mau settaggio

---
+++ mau.c
@@ -0,0 +1,8 @@
+//
+//  mau.c
+//  
+//
+//  Created by Maurizio Verduci on 17/10/2020.
+//
+
+#include "mau.h"
---
+++ mau.h
@@ -0,0 +1,13 @@
+//
+//  mau.h
+//  
+//
+//  Created by Maurizio Verduci on 17/10/2020.
+//
+
+#ifndef mau_h
+#define mau_h
+
+#include <stdio.h>
+
+#endif /* mau_h */
